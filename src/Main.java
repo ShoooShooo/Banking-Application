34df42e50d9d8080e636e0cf4c8c65aeb978505b
shahdelghazzawy <shahdelghazzawy@gmail.com>
2022-10-28 22:14:26 -0700

    commiting to github

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
 
-        public static void main(String[] args) {
-            // TODO Auto-generated method stub
-            Account account = new Account("ShooShoo","S00001");
-            account.showMenu();
-        }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Account account = new Account("ShooShoo", "S00001");
+        account.showMenu();
+    }
+}
